0576644fd95ce3718084edb50967cbcde80d7e98
rjyobal <rafaelyobal@msn.com>
2020-12-18 09:36:39 -0600

    Add full ds view and code comments

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,15 +1,25 @@
-console.log('Starting...')
+//Check JS connection
+console.log('Connection ok')
 
 // from data.js
 let tableData = data;
 //console.log(tableData)
 
 // YOUR CODE HERE!
+//Select DOM objects
 let tbody = d3.select("tbody");
 let button = d3.select("#filter-btn");
 
-button.on("click", function(){
+//Fill out HTML Table with full data set
+tableData.forEach(d=>{
+    let row = tbody.append("tr");
+    for(x in d){
+        row.append("td").text(d[x]);
+    }
+})
 
+//Create Button Filtering Function
+button.on("click", function(){
     //Clean the table
     tbody.html("");
     //Get input values
@@ -19,12 +29,11 @@ button.on("click", function(){
     //Create the fiterData Array
     let filterData = tableData.filter(x=>x.datetime===inputValDate);
     console.log(filterData)
-    //Fill out the HTML Table
+    //Fill out HTML Table with filtered data set
     filterData.forEach(d=>{
         let row = tbody.append("tr");
         for(x in d){
             row.append("td").text(d[x]);
         }
     })
-
 })
