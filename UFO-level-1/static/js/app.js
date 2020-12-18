088607b1319d38719a2d66a013476d6aabeba3ab
rjyobal <rafaelyobal@msn.com>
2020-12-18 09:26:33 -0600

    Level 1 v0.1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,30 @@
+console.log('Starting...')
+
 // from data.js
-var tableData = data;
+let tableData = data;
+//console.log(tableData)
 
 // YOUR CODE HERE!
+let tbody = d3.select("tbody");
+let button = d3.select("#filter-btn");
+
+button.on("click", function(){
+
+    //Clean the table
+    tbody.html("");
+    //Get input values
+    //Get Date Time
+    let inputValDate = d3.select("#datetime").property("value");
+    console.log(inputValDate)
+    //Create the fiterData Array
+    let filterData = tableData.filter(x=>x.datetime===inputValDate);
+    console.log(filterData)
+    //Fill out the HTML Table
+    filterData.forEach(d=>{
+        let row = tbody.append("tr");
+        for(x in d){
+            row.append("td").text(d[x]);
+        }
+    })
+
+})
